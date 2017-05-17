9d0a8f98486a4a57edca9ece4f1a2619fd2e830b
Milton Abrunhosa <mlabru@icaro.sophosoft.com.br>
2017-05-17 09:54:37 -0300

    initial release

--- build/core/WInterrupts.c.d
+++
@@ -1,6 +0,0 @@
-/home/mlabru/Public/mkr/papi/srce/papi_calibra/build/core/WInterrupts.c.o: \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/cores/arduino/WInterrupts.c \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/cores/arduino/wiring_private.h \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/cores/arduino/Arduino.h \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/cores/arduino/binary.h \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/variants/standard/pins_arduino.h
--- build/core/new.cpp.d
+++
@@ -1,2 +0,0 @@
-/home/mlabru/Public/mkr/papi/srce/papi_calibra/build/core/new.cpp.o: \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/cores/arduino/new.cpp
