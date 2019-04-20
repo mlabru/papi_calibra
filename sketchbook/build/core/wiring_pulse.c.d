e782ad21107c980d9b810aae66e042b0bbf253f9
mlabru <ml_byteman@yahoo.com.br>
2019-04-20 14:10:25 -0300

    ajustes para IEAv

--- sketchbook/build/core/wiring_pulse.c.d
+++
@@ -1,6 +0,0 @@
-/home/mlabru/Public/mkr/papi/srce/papi_calibra/sketchbook/build/core/wiring_pulse.c.o: \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/cores/arduino/wiring_pulse.c \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/cores/arduino/wiring_private.h \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/cores/arduino/Arduino.h \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/cores/arduino/binary.h \
- /home/mlabru/bin/arduino-1.8.2/hardware/arduino/avr/variants/standard/pins_arduino.h
--- sketchbook/upino.sh
+++
@@ -1,1 +0,0 @@
-./arduino-headless --upload $1/$1.ino --pref build.path=./build --board arduino:avr:uno --save-prefs --port /dev/ttyUSB0
